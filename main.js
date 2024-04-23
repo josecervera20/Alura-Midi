0a8802fd2d3c45f13ca00030bbd782d4346968a2
josecervera20 <159598106+josecervera20@users.noreply.github.com>
2024-04-22 21:52:13 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+function playSonido(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  console.log(contador);
+
+  const idAudio = `#sonido_${instrumento}`;
+  console.log(idAudio);
+
+  tecla.onclick = function () {
+    playSonido(idAudio);
+  };
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("activa");
+    }
+    console.log(evento.code === "Space" || evento.code === "Enter");
+  };
+  tecla.onkeyup = function () {
+    tecla.classList.remove("activa");
+  };
+}
+
+/* while (contador < 9) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+
+  const idAudio = `#sonido_${instrumento}`;
+  console.log(idAudio);
+
+  tecla.onclick = function () {
+    playSonido(idAudio);
+  };
+  contador = contador + 1;
+}
+ */
